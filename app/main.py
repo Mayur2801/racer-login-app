271c5b6a05683aef077222b27caf7e48d13a95ba
Mayur2801 <100698001+Mayur2801@users.noreply.github.com>
2025-06-25 17:49:38 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from models import db, User
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///racer.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+    # Add a default user if not present
+    if not User.query.filter_by(username='racer').first():
+        db.session.add(User(username='racer', password='secret'))
+        db.session.commit()
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        if username == 'racer' and password == 'secret':
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
             return 'Login successful!'
         else:
             return 'Invalid credentials', 401
